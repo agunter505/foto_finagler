99ce40ed4c52d62cd056701f83cdfc9f3428d0ee
Ann Gunter <amichele505@gmail.com>
2014-09-11 15:27:33 -0600

    Code update
    Updated functions so they are enclosed in a class to help prevent
    naming conflicts

--- foto_finagler.php
+++ foto_finagler.php
@@ -9,31 +9,40 @@
  * License: GPLv2 or later
  */
 
-/*get the image to be placed*/
-function foto_get_image() {
-	$image = "<p>This would be my image</p>";
-	echo "<div id='image_wrap'>$image</div>";
-}
-
-/*calls foto_get_image function when admin_notices is called*/
-add_action( 'admin_notices', 'foto_get_image' );
-
-/*add css for the image div*/
-function foto_css() {
-	echo "
-	<style type='text/css'>
-	#image_wrap {
-		float: right;
-		margin: 2px;
-		padding: 2px;
-		font-size: 11px;
-		background-color: red;
+//create a class to contain the image function to help prevent naming conflicts
+class get_image {
+	//get the image to be placed
+	function foto_get_image() {
+		$image = "<p>This would be my image</p>";
+		echo "<div id='image_wrap'>$image</div>";
 	}
-	</style>
-	";
 }
 
-/*add div to admin header*/
-add_action( 'admin_head', 'foto_css' );
+//calls foto_get_image function and creates a div when admin_notices is called
+add_action( 'admin_notices', array( 'get_image', 'foto_get_image' ));
+
+
+//create a class to contain the css function
+class foto_css {
+	//add css for the image div
+	function foto_css() {
+		echo "
+		<style type='text/css'>
+		#image_wrap {
+			float: right;
+			margin: 2px;
+			padding: 2px;
+			font-size: 11px;
+			background-color: red;
+		}
+		</style>
+		";
+	}
+}
+
+//create an instance of the class
+$my_foto_css = new foto_css();
+//add css to the created div to admin header
+add_action( 'admin_head', array($my_foto_css, 'foto_css' ));
 
 ?>
