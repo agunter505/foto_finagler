5b13e775f709b9be28cac6be8d7b9e5de29a82cb
Ann Gunter <amichele505@gmail.com>
2014-09-15 10:19:23 -0600

    Revert 2cfd68e..baa67b4
    This rolls back to commit 2cfd68ea473fc7399c3d5035a497bab09751482a,
    which was working.

--- foto_finagler.php
+++ foto_finagler.php
@@ -17,10 +17,9 @@ class get_image {
 		echo "<div id='image_wrap'>$image</div>";
 	}
 }
-$my_get_image = new get_image();
 
 //calls foto_get_image function and creates a div when admin_notices is called
-add_action( 'admin_notices', array( $my_get_image, 'foto_get_image' ));
+add_action( 'admin_notices', array( 'get_image', 'foto_get_image' ));
 
 
 //create a class to contain the css function
@@ -46,8 +45,6 @@ $my_foto_css = new foto_css();
 //add css to the created div to admin header
 add_action( 'admin_head', array($my_foto_css, 'foto_css' ));
 
-
-
 //begin new functions for sidebar stuff
 //loads a cute red sidebar, breaks rest of page
 function foto_sidebar_init() {
@@ -62,18 +59,29 @@ function foto_sidebar_init() {
     ) );
 }
 
-function foto_add_stylesheet() {
-	wp_register_style( 'sidebar-wrapper', plugins_url( '/foto_style.css'));
-	wp_enqueue_style( 'sidebar-wrapper' );
+
+function foto_sidebar_css() {
+	echo "<style type='text/css'>
+	.sidebar_wrapper {
+		float: right;
+		width: 20px;
+		height: 50px;
+		font-size: 11px;
+		background-color: red;
+	}
+	</style>
+	";
 }
 
-add_action ( 'admin_enqueue_scripts', 'foto_add_stylesheet');
 add_action ( 'widgets_init', 'foto_sidebar_init'); 
-
-echo " <div id=\"secondary\" class = \"sidebar_wrapper\"> ";
-echo " <div class =\"widget-area\"> ";
-dynamic_sidebar ( 'anns_sidebar-1' );
-echo " </div> ";
-echo " </div> ";
+add_action ( 'widgets_init', 'foto_sidebar_css');
 
 ?>
+
+<div id="secondary" class = "sidebar_wrapper">
+	<div class ="widget-area">
+		<?php dynamic_sidebar ( 'anns_sidebar-1' ); ?>
+	</div> <!-- space for widgets -->
+</div> <!-- end secondary -->
+
+
