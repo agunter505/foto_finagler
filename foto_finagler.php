14762f2ba9d0bc52cd4bc19f04c26f7ccc1c8dbe
Ann Gunter <amichele505@gmail.com>
2014-09-16 13:59:45 -0600

    Two steps back
    Removed sidebar stuff, gonna focus on a smaller piece of code

--- foto_finagler.php
+++ foto_finagler.php
@@ -18,6 +18,7 @@ class get_image {
 	}
 }
 
+//create an instance of the class
 $my_get_image = new get_image();
 
 //calls foto_get_image function and creates a div when admin_notices is called
@@ -46,43 +47,4 @@ class foto_css {
 $my_foto_css = new foto_css();
 //add css to the created div to admin header
 add_action( 'admin_head', array($my_foto_css, 'foto_css' ));
-
-//begin new functions for sidebar stuff
-function foto_sidebar_init() {
-    register_sidebar( array(
-        'name'          => __( 'Anns Sidebar' ),
-        'id'            => 'anns_sidebar-1',
-        'description'   => __( 'my first sidebar, lets see what happens' ),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</aside>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-    ) );
-}
-
-
-function foto_sidebar_css() {
-	echo "<style type='text/css'>
-	.sidebar_wrapper {
-		float: right;
-		width: 20px;
-		height: 50px;
-		font-size: 11px;
-		background-color: red;
-	}
-	</style>
-	";
-}
-
-add_action ( 'widgets_init', 'foto_sidebar_init'); 
-add_action ( 'widgets_init', 'foto_sidebar_css');
-
-?>
-
-<div id="secondary" class = "sidebar_wrapper">
-	<div class ="widget-area">
-		<?php dynamic_sidebar ( 'anns_sidebar-1' ); ?>
-	</div> <!-- space for widgets -->
-</div> <!-- end secondary -->
-
 
