677209f2a5ca8d50e7d7d8fb0eeef7f80fd65aa2
Ann Gunter <amichele505@gmail.com>
2014-09-16 16:22:49 -0600

    Clean up
    Relocated things, cleaned up functions

--- foto_finagler.php
+++ foto_finagler.php
@@ -16,6 +16,19 @@ class get_image {
 		$image = "<p>This would be my image</p>";
 		echo "<div id='image_wrap'>$image</div>";
 	}
+
+    //loads a cute red sidebar
+    function foto_sidebar_init() {
+        register_sidebar( array(
+            'name'          => __( 'Anns Sidebar' ),
+            'id'            => 'anns_sidebar-1',
+            'description'   => __( 'my first sidebar, lets see what happens' ),
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        ) );
+    }
 }
 
 //function to add css
@@ -29,26 +42,8 @@ $my_get_image = new get_image();
 
 //use new object to call functions then add them to admin hooks
 add_action( 'admin_notices', array( $my_get_image, 'foto_get_image' ));
+add_action ( 'widgets_init', array( $my_get_image, 'foto_sidebar_init' ));
 add_action ( 'admin_enqueue_scripts', 'foto_add_stylesheet' );
-
-
-//begin new functions for sidebar stuff
-//loads a cute red sidebar, breaks rest of page
-function foto_sidebar_init() {
-    register_sidebar( array(
-        'name'          => __( 'Anns Sidebar' ),
-        'id'            => 'anns_sidebar-1',
-        'description'   => __( 'my first sidebar, lets see what happens' ),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</aside>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-    ) );
-}
-
-add_action ( 'widgets_init', 'foto_sidebar_init');
-add_action ( 'admin-head', 'add_foto_sidebar');
-
 ?>
 
 <div id="secondary" class="sidebar_wrapper" role="complementary">
