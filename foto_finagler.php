7a047f59e8ac8c267d83a64b5848bc5883aabbb6
Ann Gunter <amichele505@gmail.com>
2014-09-19 17:27:16 -0600

    Adding an admin page
    Attempting to add and style an admin page for settings

--- foto_finagler.php
+++ foto_finagler.php
@@ -21,6 +21,10 @@ class foto_finagler {
 		add_action( 'admin_notices', array( $this, 'foto_get_image' ));
 		//hooks stylesheet to admin scripts since it's appearing in admin area
 		add_action ( 'admin_enqueue_scripts', array( $this, 'foto_add_stylesheet' ));
+		/** Step 2 (from text above). */
+		add_action( 'admin_menu', array( $this, 'my_plugin_menu' ));
+
+		include (plugin_dir_path(__FILE__) . 'views/admin.php');
 	}
 
 	//get the image to be placed and puts it in a div
@@ -31,10 +35,15 @@ class foto_finagler {
 
 	//registers and queues stylesheet for the image created above
 	public function foto_add_stylesheet() {
-		wp_register_style( 'foto_style1', plugins_url( 'foto_finagler/foto_style.css', dirname(__FILE__) ));
+		wp_register_style( 'foto_style1', plugins_url( 'foto_finagler/css/admin_foto_style.css', dirname(__FILE__) ));
 		wp_enqueue_style( 'foto_style1' );
 	}
+
+	public function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'Anns Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+	}
 }
+
 
 function run_foto_finagler() {
 	$foto_finagler = new foto_finagler();	
